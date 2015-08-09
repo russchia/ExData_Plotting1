fb1948c2f592634999a1b7aec8defffccd993481
russchia <russchia@users.noreply.github.com>
2015-08-09 09:11:22 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,13 +8,6 @@ data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
 
 # Plot 1 and save as png file
-png(file = "plot4.png", width = 480, height = 480)
-par(mfrow = c(2, 2))
-plot(data$DateTime, data$Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power")
-plot(data$DateTime, data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-plot(data$DateTime, data$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
-	lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
-	lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
-	legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=2, col=c("black", "red", "blue"))
-plot(data$DateTime, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+png(file = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
 dev.off()
